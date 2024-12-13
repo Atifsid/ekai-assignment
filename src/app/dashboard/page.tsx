8c1cc3915c5d3ddbe8f5a30ef644a41d358489c2
Atifsid <atifsiddiqui35@gmail.com>
2024-12-13 16:08:03 +0530

    add back ProtectedRoute in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,20 +9,20 @@ export default function Dashboard() {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     return (
-        // <ProtectedRoute>
-        <section className="flex flex-col md:flex-row h-screen bg-gray-100">
-            <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
-            <div className="flex-1 flex flex-col relative">
-                <button
-                    className="md:hidden absolute top-4 left-4 z-20 bg-gray-800 text-white p-2 rounded"
-                    onClick={() => setIsSidebarOpen(true)}
-                >
-                    <MdEditSquare />
-                </button>
-                {/* <Avatar /> */}
-                <Chat />
-            </div>
-        </section>
-        // </ProtectedRoute>
+        <ProtectedRoute>
+            <section className="flex flex-col md:flex-row h-screen bg-gray-100">
+                <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+                <div className="flex-1 flex flex-col relative">
+                    <button
+                        className="md:hidden absolute top-4 left-4 z-20 bg-gray-800 text-white p-2 rounded"
+                        onClick={() => setIsSidebarOpen(true)}
+                    >
+                        <MdEditSquare />
+                    </button>
+                    {/* <Avatar /> */}
+                    <Chat />
+                </div>
+            </section>
+        </ProtectedRoute>
     )
 }
